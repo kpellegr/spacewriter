9283443b3fb14c49c689ca11b35f6c79b804fa1a
wannes <wannesghielens@gmail.com>
2017-02-14 00:46:45 +0100

    added planet placeholders to menu

--- js/levelsmenu.js
+++ js/levelsmenu.js
@@ -10,17 +10,47 @@ class LevelsMenu extends Menu {
 	preload(){
 		super.preload();
 
+		this.game.load.atlasXML('level_atlas', 'asset/sprites/planets.png', 'asset/sprites/planets.xml');
 	    this.game.load.text('levels', 'asset/data/levels.json');
 	    var levels = JSON.parse(this.game.cache.getText('levels')).levels;
-
-	    this.menu = [{title: "<- BACK", action: this.cb(() => this.app.router.showMenu()) }];
-
 	    var startLevel = this.cb(l => this.startLevel(l));
 
-	    levels.forEach(this.cb(l => this.menu.push({
+	    this.menu = levels.map(this.cb(l => ({
 	    	title: l.name,
 	    	action: (function(level){ return () => startLevel(level); })(l)
 	    })));
+	}
+
+	create(){
+		// Draw background
+		this.starfield = this.game.add.tileSprite(0, 0, this.width, this.height, 'background');
+
+		var offset = 150;
+		this.menu.forEach(this.cb(function(item, index){
+			var levelSprite = this.game.add.sprite(this.width*(index % 2 == 0 ? .25 : .75), this.height - offset, 'level_atlas', 'level'+index+'.png');
+			levelSprite.anchor.x = .5;
+			levelSprite.anchor.y = .5;
+
+			if(index > 0){
+				levelSprite.alpha = .5;
+			}else{
+				levelSprite.inputEnabled = true;
+				levelSprite.events.onInputDown.add(item.action, this);
+			}
+
+			offset += 100;
+		}));
+
+
+		var g = this.game.add.graphics(0, 0);
+		g.beginFill("black")
+		g.drawRect(0, 0, this.width, 100);
+		g.endFill();
+		g.inputEnabled = true;
+		g.events.onInputDown.add(this.cb(() => this.app.router.showMenu()), this);
+
+		var text = this.game.add.text(0, 0, "<- BACK", { font: "20px Arial Black", fill: "#eeeeee", boundsAlignH: "center", boundsAlignV: "middle" });
+		text.setTextBounds(0, 0, this.width, 100);
 	}
 
 	startLevel(level){
