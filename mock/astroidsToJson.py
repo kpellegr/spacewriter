57c1dcfc1a78270ddd21e0055fec69db957767be
wannes <wannesghielens@gmail.com>
2017-02-20 14:34:56 +0100

    added mock data generated from astroid list

--- mock/astroidsToJson.py
+++ mock/astroidsToJson.py
@@ -15,12 +15,15 @@ def getSampleLevel(i):
 	}
 
 levels = []
+translations_en = {}
 
 i = 1
 for line in open("astroidlist.txt", "r"):
 	if len(line.split("\t")) >= 2:
 		sampleLevel = getSampleLevel(i)
 		levels.append(sampleLevel)
+
+		translations_en[sampleLevel["name"]] = line.split("\t")[0]
 
 		i += 1
 	elif len(line.strip()) > 0 and "=" not in line:
@@ -31,9 +34,14 @@ for line in open("astroidlist.txt", "r"):
 		sampleLevel["checkpoint"] = True
 		levels.append(sampleLevel)
 
+		translations_en[sampleLevel["name"]] = line.strip()
+
 		i += 1
 
 with open("levels.json", "w+") as out:
 	out.write(json.dumps({
 		"levels": levels
 	}, indent=4))
+
+with open("translate_asteroids_en.json", "w+") as out:
+	out.write(json.dumps(translations_en, indent=4, sort_keys=True))
